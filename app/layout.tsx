22857bf31ce68cda4d0785992f369c96e5f7ce66
ewnorman03 <153875000+ewnorman03@users.noreply.github.com>
2026-03-24 21:11:44 -0230

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css';
+import {inter} from '@/app/ui/fonts'
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased bg-gray-900 text-gray-100`}>{children}</body>
     </html>
   );
 }
